ccb1138b7f4f9d6a12b3653ebcf07251b0705d9b
venqics <venx.rao87@gmail.com>
2021-05-26 16:58:50 +0530

    #1.2 querying and methods on rooms

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,15 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+from . import views
 
 app_name = "rooms"
+router = DefaultRouter()
+router.register("", views.RoomViewSet)
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/v1/rooms/", include("rooms.urls")),
-    path("api/v1/users/", include("users.urls")),
-]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = router.urls
